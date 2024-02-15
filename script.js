cfb84a5612b8ae797965d708e92a9ea31ef18c40
Elijah Gummer <elijahgummer5@gmail.com>
2024-02-15 20:53:22 +1000

    added a nice little ease in ease out transition when the user scrolls beyond a y point using js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const navbar = document.querySelector(".navbar");
-    const menuBtn = document.querySelector(".menu-btn");
-    const cancelBtn = document.querySelector(".cancel-btn");
-    const phoneBtn = document.querySelector(".phone-btn");
-    menuBtn.onclick = ()=>{
-      navbar.classList.add("show");
-      menuBtn.classList.add("hide");
-      
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const phoneBtn = document.querySelector(".phone-btn");
+menuBtn.onclick = () => {
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+};
+cancelBtn.onclick = () => {
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+};
+
+let scrollTimeout;
+
+window.onscroll = function () {
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(function () {
+    var currentScrollPos = window.pageYOffset;
+    var contactRow = document.querySelector(".contact-row");
+    if (currentScrollPos < 100) {
+      contactRow.style.display = "block";
+      navbar.style.top = "60px";
+    } else {
+      contactRow.style.display = "none";
+      navbar.style.top = "0";
     }
-    cancelBtn.onclick = ()=>{
-      navbar.classList.remove("show");
-      menuBtn.classList.remove("hide");
-    }
+  }, 100); // Adjust debounce delay as needed (in milliseconds)
+};
