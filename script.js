7de42f185d4d379b2196e2943b16a03f8ae9ace7
Elijah Gummer <elijahgummer5@gmail.com>
2024-03-13 09:48:19 +1000

    changed the review content

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
     {
       name: "Katey T.",
       testimonial:
-        "Elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla",
+        "",
     },
     {
       name: "Jae Robin",
