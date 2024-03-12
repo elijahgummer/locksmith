97d1a30a498d998418f903db893fae72e5e6327e
Elijah Gummer <elijahgummer5@gmail.com>
2024-03-13 09:44:46 +1000

    got rid of the job object in th js array

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
   const testimonials = [
     {
       name: "Eva Sawyer",
-      job: "CEO, Fashworks",
       testimonial:
         "Neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur",
     },
