86c26dce2119b1f6f9f4b1a710c7e96d09407fbe
Elijah Gummer <elijahgummer5@gmail.com>
2024-03-13 09:51:46 +1000

    changed the review content

--- script.js
+++ script.js
@@ -33,11 +33,6 @@ document.addEventListener("DOMContentLoaded", function() {
       testimonial:
         "As an office manager, I can wholeheartedly recommend locksmith. I have more than 100 locks in my office, and locksmith has never ever let me down.",
     },
-    {
-      name: "Nicola Blakely",
-      testimonial:
-        "Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
-    },
   ];
 
   //Current Slide
